82e7443c1bfdee98496f1512d30b22e585627771
Elizaveta Martirosyan <ermartirosyan@edu.hse.ru>
2021-09-30 04:22:12 +0300

    Update checker.cpp

--- checker.cpp
+++ checker.cpp
@@ -1,67 +1,76 @@
-#include <bits/stdc++.h>
+double my_solve(int n, int k, vector<pair<double, double>> &d) {
 
-#define int long long
-#define pii pair<int, int>
-#define all(x) (x).begin(), (x).end()
-#define sq(x) (x) * (x)
-#define double long double
-#define fi first
-#define se second
+    vector<pair<int, int>> v;
 
-using namespace std;
+    v.reserve(d.size());
+    for (auto &i : d)
+        v.emplace_back(abs(i.first), abs(i.second));
 
-double solve_true(int n, int k, vector<pii > v) {
-    double ans = 100000000;
-    for (double x = -1000.; x <= 1000.; x += 0.001) {
-        vector<double> q;
-        for (int i = 0; i < v.size(); i++)
-            q.push_back(sqrt((double) (sq(x - v[i].fi) + sq(v[i].se))));
-        sort(all(q));
-        if (ans > q[k - 1]) {
-            ans = q[k - 1];
+    double ans = 1500.;
+    double l = 0, r = ans, eps = 0.001;
+    while (r - l >= 0.0001) {
+        double c = (l + r) / 2;
+
+        vector<pair<double, pii>> cnts;
+        for (int i = 0; i < v.size(); i++) {
+            if (sq(c) < sq(v[i].se))
+                continue;
+            double s = sqrt(sq(c) - sq(v[i].se));
+            if (s != 0) {
+                cnts.push_back({v[i].fi - s, {i, 1}});
+                cnts.push_back({v[i].fi + s, {i, 0}});
+            } else
+                cnts.push_back({v[i].fi + s, {i, 2}});
         }
+
+        sort(all(cnts));
+
+        auto comp = [](pair<double, pii > a, pair<double, pii > b) {
+            if (a.second.second != b.second.second)
+                return a.second.second < b.second.second;
+            if (a.first == b.first)
+                return a.second.first < b.second.first;
+            return a.first < b.first;
+        };
+
+        set<pair<double, pii >, decltype(comp)> yes(comp);
+
+        map<int, int> in;
+        int i = 0;
+        while (i < cnts.size() && cnts[i].fi <= 0) {
+            yes.insert(cnts[i]);
+            in[cnts[i].second.first] += 1;
+            i += 1;
+        }
+
+        for (; i < cnts.size(); i++) {
+            if (in.size() >= k)
+                break;
+            while (!yes.empty() && yes.rbegin()->second.second == 2) {
+                auto it = yes.end();
+                it--;
+                yes.erase(it);
+            }
+            in[cnts[i].se.first] += 1;
+            yes.insert(cnts[i]);
+            if (in[cnts[i].se.fi] == 2) {
+                in[cnts[i].se.first] -= 1;
+                yes.erase({cnts[i].fi, {cnts[i].se.fi, 1 - cnts[i].se.se}});
+            }
+            while (!yes.empty() && yes.begin()->se.se == 0 && yes.begin()->first < cnts[i].fi) {
+                in[yes.begin()->se.fi] -= 1;
+                if (in[yes.begin()->se.fi] == 0)
+                    in.erase(yes.begin()->se.fi);
+                yes.erase(yes.begin());
+            }
+        }
+
+        if (in.size() >= k) {
+            ans = min(c, ans);
+            r = c - eps;
+        } else
+            l = c + eps;
     }
+
     return ans;
-}
-
-double my_solve(size_t n_len, size_t k_len, vector<double> x, vector<double> y){
-    return 0.;
-}
-
-signed main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
-    cout.tie(nullptr);
-
-//    freopen("input.txt", "r", stdin);
-//    freopen("output.txt", "w", stdout);
-
-    srand(time(nullptr));
-
-    int n = 5, k = rand() % n + 1;
-
-
-    vector<pii > d;
-    vector<double> x;
-    vector<double> y;
-    for (int i = 0; i < n; i++) {
-        int xx, yy;
-        xx = rand() % 2000 - 1000;
-        yy = rand() % 2000 - 1000;
-        d.push_back({xx, yy});
-        x.push_back(xx);
-        y.push_back(yy);
-    }
-
-    cout << n << " " << k << "\n";
-    for (auto i : d)
-        cout << "{" << i.fi << ", " << i.se << "}," << "\n";
-
-
-    cout << n << " " << k << "\n";
-    for (auto i : d)
-        cout << "" << i.fi << " " << i.se << " " << "\n";
-
-    cout << solve_true(n, k, d) << "\n";
-    cout << my_solve(n, k, x, y) << "\n";
 }
